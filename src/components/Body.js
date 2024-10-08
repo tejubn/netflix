9d577f9e1c64d53e54711c44f64cbb413d0bbf65
tejubn <mail2tejubn06@gmail.com>
2024-10-08 20:23:05 +0530

    body and browse updated

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,10 +2,6 @@ import { createBrowserRouter } from "react-router-dom";
 import Browse from "./Browse";
 import Login from "./Login";
 import { RouterProvider } from "react-router-dom";
-<<<<<<< HEAD
-=======
-
->>>>>>> 53466590d10f2e963122ee7227363570d6a5f0c9
 const Body = () => {
   const appRouter = createBrowserRouter([
     {
--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,5 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
 import Header from './Header';
 import useNowPlayingMovies from '../hooks/useNowPlayingMovies';
 import MainContianer from './MainContianer';
@@ -14,12 +13,6 @@ const Browse = () => {
       <MainContianer/>
       <SecondaryContainer/>
     </div>
-=======
-
-const Browse = () => {
-  return (
-    <div>Browse</div>
->>>>>>> 53466590d10f2e963122ee7227363570d6a5f0c9
   )
 }
 
